745385dc7c9fdefa5e593512607135cf76370e54
HaidarKhalid <HaidarKhalid01@gmail.com>
2023-01-05 01:10:57 +0300

    bug fixed

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 let randomEl = document.getElementById("random")
 let randomNum = Math.float(Math.random() * 3)
-randomEl.innerText = "your random number is --> " + randomNum
+randomEl.textContent = "your random number is --> " + randomNum
